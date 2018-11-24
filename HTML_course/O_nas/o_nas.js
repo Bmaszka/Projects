51c18cb90f36a9e74c21edb3a18d29f314db0c4e
Bartek <BartMaszka@gmail.com>
2018-11-24 13:57:51 +0100

    js

---
+++ HTML_course/O_nas/o_nas.js
@@ -0,0 +1,12 @@
+function dlaOnas (){
+	document.getElementById('opis').innerHTML = '<H1>O nas</H1>';
+	document.getElementById('michal').innerHTML = '<H1>Michał</H1>'; 
+}
+function dlaOnas2 (){
+	document.getElementById('opis').innerHTML = '<H1>O nas</H1>';
+	document.getElementById('bartek').innerHTML = '<H1>Bartek</H1>'
+}
+function dlaOnas3 () {
+	document.getElementById('opis').innerHTML = '<H1>O nas</H1>';
+	 document.getElementById('dominik').innerHTML = '<H1>Dominik</H1>'
+}
---
+++ HTML_course/O_nas/o_nas2.js
@@ -0,0 +1,4 @@
+function dlaOnas (){
+	document.getElementById('opis').innerHTML = '<H1>O nas</H1>';
+	document.getElementById('bartek').innerHTML = '<H1>Bartek</H1>'
+}
