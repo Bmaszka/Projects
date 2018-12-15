6f6d98c97dc404fc36704e9de5137156eeb3e701
Bartek <BartMaszka@gmail.com>
2018-12-15 13:44:48 +0100

    Pixel

--- PixelArt/scripts.js
+++ PixelArt/scripts.js
@@ -1,4 +1,5 @@
 var penColor = "black"
+var isHeader = 0
 
 function setPixelColor(pixel)
 {
@@ -18,4 +19,28 @@ function setCurrentColor (color)
 {
 	var currentColor= document.getElementById("currentcolor");
 	currentColor.style.backgroundColor = color;
+}
+
+function setText()
+{
+	if (isHeader==0)
+	{
+	document.getElementById("palette").innerHTML = '<h1 id="paletteHeader">Paleta</h1>' + document.getElementById("palette").innerHTML;
+     isHeader  = 1;
+	}
+	else
+	{
+	document.getElementById("paletteHeader").remove();
+	isHeader=0;
+	}
+}
+function addRow()
+{
+		var rowCollection = document.getElementsByClassName("row");
+		document.getElementById("art").innerHTML = document.getElementById("art").innerHTML + '<div class="row">' + rowCollection[0].innerHTML + '</div>';
+}
+function removeRow()
+{
+		var rowCollection = document.getElementsByClassName("row");
+		rowCollection[0].remove();
 }
