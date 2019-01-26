17bc2fabf40f60de611e6311c0f265b2cf8c0d49
Bartek <BartMaszka@gmail.com>
2019-01-26 15:52:18 +0100

    Fibonnaci

--- Algorytmy/silnia.js
+++ Algorytmy/silnia.js
@@ -1,4 +1,4 @@
-/*function silnia(n)
+function silnia(n)
 {
 	var s;
 	var licznik ;
@@ -6,15 +6,53 @@
 		if (n>=0)
 		{
 			s=1;
-			for (licznik=1; licznik<=n; licznik++)
+			if (n>0)
 			{
-				s=s*licznik;
-			}
+				for (licznik=1; licznik<=n; licznik++)
+				{
+					s=s*licznik;
+				}
+			}			
 			return s;
 		}
 		return "działanie nie możliwe";
+}	
+function obliczS()
+{
+	var x,silniaW, text;
+window.alert("silnia")
+	x = document.getElementById("liczba").value;
+	silniaW = silnia(x);
+	text = "Silnia Liczby " + x + " wynosi :" + silniaW;
+	document.getElementById("wynik").innerHTML = text;
+}	
+
+function fib(f)
+{
+	var a=1, b=0, temp;
+	while (f>=0)
+	{
+		temp = a;
+		a = a + b;
+		b = temp;
+		f--;
+		document.getElementById("wynik").innerHTML = document.getElementById("wynik").innerHTML + "</br>"+ b;
+	}
+	
+return b;
 }
-*/
+
+function obliczF()
+{
+	var x, FibO, text;
+
+	x = document.getElementById("liczba").value;
+	FibO = fib(x);
+	text = "Ciąg F " + x + " wynosi :" + FibO;
+	document.getElementById("wynik").innerHTML =document.getElementById("wynik").innerHTML + "</br>"+ text;
+}
+
+/*
 function silnia(n)
 {
 	if (n>=0) 
@@ -33,3 +71,4 @@ function silnia(n)
 			return "Tylko liczby całkowite >= 0";
 		}
 	};
+	*/
